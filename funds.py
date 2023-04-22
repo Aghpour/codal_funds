41f95a168dcee95bba74c4cde71bccaca392c0cc
Aghpour <76126008+Aghpour@users.noreply.github.com>
2023-04-22 14:14:25 +0330

    Update funds.py

--- funds.py
+++ funds.py
@@ -10,7 +10,7 @@ from IPython.display import clear_output
 logging.basicConfig(level=logging.INFO)
 
 headers = {"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0"}
-symbol = 'ارزش'
+symbol = 'کاردان'    # put your desired symbol
 download_failed = []
 main_dir = str(Path.home() / "Downloads")
 
